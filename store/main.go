b32024ee7f005b96f236719a28b58eb4ae305900
Andrew Delph <andrewdelph@live.ca>
2023-08-04 12:40:49 -0400

    store zk client and server

--- store/main.go
+++ store/main.go
@@ -3,9 +3,12 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"math/rand"
 	"net/http"
 	"time"
+
+	"github.com/go-zookeeper/zk"
 )
 
 // Global map
@@ -50,8 +53,62 @@ func baseHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Server ID: %s\n", serverID)
 }
 
-func main() {
+// func main() {
 
+// 	rand.Seed(time.Now().UnixNano())
+// 	serverID = fmt.Sprintf("Server-%d", rand.Int())
+
+// 	http.HandleFunc("/", baseHandler)
+
+// 	http.HandleFunc("/add", addHandler)
+// 	http.HandleFunc("/get", getHandler)
+// 	http.HandleFunc("/list", listHandler)
+
+// 	fmt.Println("Server is running on port 8080...")
+// 	if err := http.ListenAndServe(":8080", nil); err != nil {
+// 		panic(err)
+// 	}
+// }
+
+func watchGroup(conn *zk.Conn, path string) {
+	for {
+		children, _, watchChannel, err := conn.ChildrenW(path)
+		if err != nil {
+			log.Fatalf("Failed to list children for group %s: %v", path, err)
+			return
+		}
+		log.Printf("Group members: %v", children)
+
+		// Wait for changes in the group
+		<-watchChannel
+		log.Println("Group membership has changed!")
+	}
+}
+
+func main() {
+	log.Println("STARTING STARTING STARTING STARTING STARTING ")
+	servers := []string{"zookeeper:2181"}
+	conn, _, err := zk.Connect(servers, time.Second)
+	if err != nil {
+		log.Fatalf("Unable to connect to ZooKeeper: %v", err)
+	}
+	defer conn.Close()
+
+	groupPath := "/key-store"
+
+	// Register the client to the group
+	clientPath := groupPath + "/client-"
+	_, err = conn.CreateProtectedEphemeralSequential(clientPath, []byte{}, zk.WorldACL(zk.PermAll))
+	if err != nil {
+		log.Fatalf("Unable to register client to the group: %v", err)
+	}
+
+	log.Println("Successfully registered to the key-store group!")
+
+	// Watch for changes in the group
+	go watchGroup(conn, groupPath)
+
+	// Start the http server
 	rand.Seed(time.Now().UnixNano())
 	serverID = fmt.Sprintf("Server-%d", rand.Int())
 
@@ -66,45 +123,3 @@ func main() {
 		panic(err)
 	}
 }
-
-// func watchGroup(conn *zk.Conn, path string) {
-// 	for {
-// 		children, _, watchChannel, err := conn.ChildrenW(path)
-// 		if err != nil {
-// 			log.Fatalf("Failed to list children for group %s: %v", path, err)
-// 			return
-// 		}
-// 		log.Printf("Group members: %v", children)
-
-// 		// Wait for changes in the group
-// 		<-watchChannel
-// 		log.Println("Group membership has changed!")
-// 	}
-// }
-
-// func main() {
-// 	log.Println("STARTING STARTING STARTING STARTING STARTING ")
-// 	servers := []string{"zookeeper:2181"}
-// 	conn, _, err := zk.Connect(servers, time.Second)
-// 	if err != nil {
-// 		log.Fatalf("Unable to connect to ZooKeeper: %v", err)
-// 	}
-// 	defer conn.Close()
-
-// 	groupPath := "/key-store"
-
-// 	// Register the client to the group
-// 	clientPath := groupPath + "/client-"
-// 	_, err = conn.CreateProtectedEphemeralSequential(clientPath, []byte{}, zk.WorldACL(zk.PermAll))
-// 	if err != nil {
-// 		log.Fatalf("Unable to register client to the group: %v", err)
-// 	}
-
-// 	log.Println("Successfully registered to the key-store group!")
-
-// 	// Watch for changes in the group
-// 	go watchGroup(conn, groupPath)
-
-// 	// Keep the connection open
-// 	time.Sleep(10 * time.Minute)
-// }
