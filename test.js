37fd5a5ce2318cec900b00922ce96970a4f166c5
Andrew Delph <andrewdelph@live.ca>
2023-08-07 17:05:03 -0400

    test

--- test.js
+++ test.js
@@ -6,8 +6,8 @@ import { randomString } from "https://jslib.k6.io/k6-utils/1.2.0/index.js";
 import { textSummary } from "https://jslib.k6.io/k6-summary/0.0.2/index.js";
 
 export let options = {
-  vus: 100,
-  duration: "5m",
+  vus: 5,
+  duration: "20s",
   // iterations: 30,
   // scenarios: {
   //   // disrupt: {
