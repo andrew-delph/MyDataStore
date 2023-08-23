d6f9fc880ada2190d64b960c03633d01cc92334d
Andrew Delph <andrewdelph@live.ca>
2023-08-23 13:08:15 -0400

    test(store): add test for partition verify queue

---
+++ store/manager_test.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"container/heap"
+	"testing"
+
+	"github.com/sirupsen/logrus"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPartitionVerifyQueue(t *testing.T) {
+	// Some items and their priorities.
+	items := map[int]int{
+		1: 3, 2: 2, 3: 1,
+	}
+
+	// Create a priority queue, put the items in it, and
+	// establish the priority queue (heap) invariants.
+	pq := make(PartitionEpochQueue, len(items))
+	i := 0
+	for partitionId, epoch := range items {
+		pq[i] = &PartitionEpochItem{
+			epoch:       int64(epoch),
+			partitionId: partitionId,
+			index:       i,
+		}
+		i++
+	}
+	heap.Init(&pq)
+
+	// Insert a new item and then modify its priority.
+	item := &PartitionEpochItem{
+		epoch:       6,
+		partitionId: 6,
+	}
+	pq.Push(item)
+	// heap.Push(&pq, item)
+
+	popped := heap.Pop(&pq).(*PartitionEpochItem)
+
+	assert.EqualValues(t, 1, popped.epoch, "popped.epoch wrong value")
+	assert.EqualValues(t, 3, popped.partitionId, "popped.partitionId wrong value")
+
+	// Take the items out; they arrive in decreasing priority order.
+	for pq.Len() > 0 {
+		item := heap.Pop(&pq).(*PartitionEpochItem)
+		logrus.Infof("partitionId = %d epoch = %d", item.partitionId, item.epoch)
+	}
+}
