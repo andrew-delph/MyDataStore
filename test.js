6ef487f22175b9c44e7a57242e3a3c35889afd03
Andrew Delph <andrewdelph@live.ca>
2023-08-31 17:39:22 -0400

    update pprof command

--- test.js
+++ test.js
@@ -146,7 +146,7 @@ export function remove() {
   return;
 }
 
-options = { duration: "2h", vus: 5 };
+options = { duration: "2h", vus: 1 };
 // options = { iterations: 1, vus: 1 };
 export default function () {
   // leader();
