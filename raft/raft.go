243646cb39274c0efe321c8001f25dd3209eda4a
Andrew Delph <andrewdelph@live.ca>
2023-09-06 10:37:16 -0400

    init raft package

---
+++ raft/raft.go
@@ -0,0 +1,9 @@
+package raft
+
+import (
+	"github.com/sirupsen/logrus"
+)
+
+func raftTest() {
+	logrus.Warn("RAFT")
+}
---
+++ raft/raft_test.go
@@ -0,0 +1,9 @@
+package raft
+
+import (
+	"testing"
+)
+
+func TestRaftDefault(t *testing.T) {
+	raftTest()
+}
