3d13faaf7b24f52511975ff3d9f7414020a47e48
Andrew Delph <andrewdelph@live.ca>
2023-09-08 14:36:00 -0400

    remove execution logs from tools

--- tools/gopackagesdriver.sh
+++ tools/gopackagesdriver.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 
-exec bazel run --execution_log_json_file=tools-events.json --tool_tag=gopackagesdriver -- @io_bazel_rules_go//go/tools/gopackagesdriver "${@}"
+exec bazel run --tool_tag=gopackagesdriver -- @io_bazel_rules_go//go/tools/gopackagesdriver "${@}"
 
