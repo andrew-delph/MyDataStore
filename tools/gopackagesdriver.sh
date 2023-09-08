e31341373d481ac0ec7306a09ccc75739a917a0d
Andrew Delph <andrewdelph@live.ca>
2023-09-07 20:37:00 -0400

    tools logs to events.json

--- tools/gopackagesdriver.sh
+++ tools/gopackagesdriver.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 
-exec bazel run --tool_tag=gopackagesdriver -- @io_bazel_rules_go//go/tools/gopackagesdriver "${@}"
+exec bazel run --execution_log_json_file=events.json --tool_tag=gopackagesdriver -- @io_bazel_rules_go//go/tools/gopackagesdriver "${@}"
 
