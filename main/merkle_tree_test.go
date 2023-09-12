3f491db36722527e6df0092647497fc9357908c3
Andrew Delph <andrewdelph@live.ca>
2023-09-11 20:23:40 -0400

    TestMerkleTree has own manager config

--- main/merkle_tree_test.go
+++ main/merkle_tree_test.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"testing"
 
+	"github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/assert"
 
+	"github.com/andrew-delph/my-key-store/config"
 	"github.com/andrew-delph/my-key-store/rpc"
 )
 
@@ -14,7 +17,17 @@ func TestMerkleTree(t *testing.T) {
 	// if testing.Short() {
 	// 	t.Skip("skipping test in short mode.")
 	// }
-	manager := createMockManager(t)
+	tmpDir, err := os.MkdirTemp("", "my-key-store")
+	if err != nil {
+		t.Fatal("Error creating temporary directory:", err)
+	}
+	logrus.Info("Temporary Directory:", tmpDir)
+	c := config.GetConfig()
+	c.Storage.DataPath = tmpDir
+	c.Manager.PartitionCount = 1
+	c.Manager.PartitionBuckets = 100
+
+	manager := NewManager(c)
 
 	writeValuesNum := 10
 
@@ -67,6 +80,8 @@ func TestMerkleTree(t *testing.T) {
 	if err != nil {
 		t.Error(err)
 	}
+
+	assert.EqualValues(t, c.Manager.PartitionBuckets, len(tree3.Leafs), "checking content size")
 
 	assert.NotEqualValues(t, tree1.MerkleRoot(), tree3.MerkleRoot(), "hash should be the same")
 }
