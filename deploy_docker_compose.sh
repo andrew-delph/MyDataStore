0381e1120ff2c789685483ab3f0adc9b2071045a
Andrew Delph <andrewdelph@live.ca>
2023-09-19 16:47:18 -0400

    docker-compose down to clear prom data

--- deploy_docker_compose.sh
+++ deploy_docker_compose.sh
@@ -7,7 +7,8 @@ echo "STARTING DEPLOY DOCKER-COMPOSE"
 
 bazel run --execution_log_json_file=events.json //main:store_image
 
-docker-compose up --force-recreate -d 
+docker-compose down
+docker-compose up --force-recreate --remove-orphans -d  
 
 
 
