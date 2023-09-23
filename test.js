9ae2c92ef7b11e21274a55af5d606110af89d307
Andrew Delph <andrewdelph@live.ca>
2023-09-23 13:58:47 -0400

    increase test duration

--- test.js
+++ test.js
@@ -20,7 +20,7 @@ export let options = {
     constant_arrival: {
       executor: "constant-arrival-rate",
       // How long the test lasts
-      duration: "40m",
+      duration: "2h",
       // How many iterations per timeUnit
       rate: 50,
       // Start `rate` iterations per second
