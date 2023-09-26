20a49fad20026848eeb6c0d02af788f6401cc931
Andrew Delph <andrewdelph@live.ca>
2023-09-26 10:00:05 -0400

    e2e working

--- e2e/run-kube.sh
+++ e2e/run-kube.sh
@@ -9,17 +9,6 @@ if [ $# -lt 1 ]; then
 fi
 
 
-if [ "$2" == "--external" ]; 
-then
-    echo "USING EXTERNAL"
-    WS_HOST="ws:\/\/ws.andrewdelph.com\/socket.io\/\?EIO\=4\&transport\=websocket"
-    OPTIONS_HOST="https:\/\/options.andrewdelph.com"
-else
-    echo "USING INTERNAL"
-    WS_HOST="ws:\/\/socketio-service.default.svc.cluster.local\/socket.io\/\?EIO\=4\&transport\=websocket"
-    OPTIONS_HOST="http:\/\/options-service.default.svc.cluster.local"
-fi
-
 RESOURCE_FILENAME=$1
 RESOURCE_NAME="$(basename -s .yaml $RESOURCE_FILENAME)"
 # Inspect the YAML to find the actual test-script name...
@@ -28,18 +17,12 @@ SCRIPT_NAME="$(yq -r '.spec.script.configMap.file' $RESOURCE_FILENAME)"
 TAG_PREFIX="$(basename -s .js $SCRIPT_NAME)"
 TAG_NAME="test-$(date +%m-%d-%H-%M-%S)"
 
-
 # Replacement doesn't seem to trigger, so we need to delete any previous execution
-kubectl --context minikube delete --ignore-not-found=true --wait=true -f $RESOURCE_FILENAME -n k6
+kubectl delete --ignore-not-found=true --wait=true -f $RESOURCE_FILENAME -n k6
 
 # Update '--tag testid=...' to include the test-script name and timestamp for uniqueness, then apply
-sed -e "s/testid\=${RESOURCE_NAME}/testid\=${TAG_NAME}/g" -e "s/WS_HOST_VALUE/${WS_HOST}/g" -e "s/OPTIONS_HOST_VALUE/${OPTIONS_HOST}/g" $RESOURCE_FILENAME | kubectl --context minikube apply  -f - -n k6
+sed -e "s/testid\=${RESOURCE_NAME}/testid\=${TAG_NAME}/g" $RESOURCE_FILENAME | kubectl  apply  -f - -n k6
 
-# sed -e "s/testid\=${RESOURCE_NAME}/testid\=${TAG_NAME}/g" -e "s/WS_HOST_VALUE/${WS_HOST}/g" -e "s/OPTIONS_HOST_VALUE/${OPTIONS_HOST}/g" $RESOURCE_FILENAME
-
-echo "WS_HOST:" $WS_HOST
-
-echo "OPTIONS_HOST:" $OPTIONS_HOST
 
 echo "RESOURCE_NAME:" $RESOURCE_NAME
 
