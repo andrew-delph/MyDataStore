8d780cd94b16a15c9c2f4cf30ced22dd8f544180
Andrew Delph <andrewdelph@live.ca>
2023-10-03 15:39:05 -0400

    verify attempts order working in heap

--- main/consistency_heap_test.go
+++ main/consistency_heap_test.go
@@ -11,18 +11,25 @@ import (
 func TestConsistencyHeap(t *testing.T) {
 	h := NewConsistencyHeap()
 
-	h.PushSyncTask(1, int64(2))
-	h.ManualPush(1, 2, false, 3)
-	h.PushSyncTask(1, int64(3))
-	h.ManualPush(1, 2, false, 2)
-	h.PushSyncTask(1, int64(1))
-	h.ManualPush(1, 2, false, 4)
-	h.PushVerifyTask(1, int64(1))
-	h.PushVerifyTask(1, int64(5))
+	for i := 0; i < 10; i++ {
+		h.PushSyncTask(1, int64(2))
+		h.ManualPush(1, 2, false, 3)
+		h.PushSyncTask(1, int64(3))
+		h.ManualPush(1, 2, false, 2)
+		h.PushSyncTask(1, int64(1))
+		h.ManualPush(1, 2, false, 4)
+		h.PushVerifyTask(1, int64(1))
+		h.PushVerifyTask(1, int64(5))
+	}
 
 	assert.Equal(t, true, true, "true")
+	minAttempt := 0
 	for h.Len() > 0 {
 		popped := h.PopItem()
+		if popped.Attemps < minAttempt {
+			t.Error("attempts order broken")
+		}
+		minAttempt = popped.Attemps
 		logrus.Debugf("popped= %+v len=%d", popped, h.Len())
 	}
 
