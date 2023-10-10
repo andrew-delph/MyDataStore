f0ae1bbc92b93d727ff596d3b200a837bfbb1508
Andrew Delph <andrewdelph@live.ca>
2023-10-10 16:40:13 -0400

    100 rate

--- e2e/test.js
+++ e2e/test.js
@@ -23,7 +23,7 @@ export let options = {
       // How long the test lasts
       duration: "2h",
       // How many iterations per timeUnit
-      rate: 10,
+      rate: 100,
       // Start `rate` iterations per second
       timeUnit: "1s",
       // Pre-allocate VUs
